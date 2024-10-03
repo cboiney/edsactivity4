d8256210fd902e306205625d0e1f54de6a1d74d5
cboiney <cboiney@hamilton.edu>
2024-10-03 14:38:33 +0000

    2

---
+++ main.R
@@ -0,0 +1,9 @@
+#install.packages(c("lubridate", "dplyr", "ggplot2"))
+library(lubridate)
+library(dplyr)
+library(ggplot2)
+
+weather <- read.csv("/cloud/project/campus_weather.csv")
+
+print("prontly")
+print("test")
